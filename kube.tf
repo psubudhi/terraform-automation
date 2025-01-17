d5297abbb9ddc63957e744cdc279aa8be95df42f
vaibhav jaiswal <vaibhavj@gmail.com>
2025-01-17 16:13:03 +0530

    helm

--- kube.tf
+++ kube.tf
@@ -1,10 +1,6 @@
 provider "helm" {
   kubernetes {
-    host                   = data.terraform_remote_state.eks.outputs.EKS_cluster_endpoint
-#    token                  = data.terraform_remote_state.eks.outputs.EKS_cluster_auth_token
-    cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.EKS_cluster_CA_data)
-    config_path = "~/.kube/config"
-  # load_config_file       = false
+    config_path = "~/.kube/config"  # Path to your Kubernetes config file
   }
 }
  
