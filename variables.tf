1bd23bfa02a88c996a30d4b7c6bdb03303978e1b
vaibhav jaiswal <vaibhavj@gmail.com>
2025-01-17 16:38:11 +0530

    cluster-name

--- variables.tf
+++ variables.tf
@@ -3,3 +3,9 @@ variable "region" {
   type        = string
   default     = "ap-south-1"
 }
+
+variable "cluster_name" {
+  description = "Kubernetes Cluster Name"
+  type        = string
+  default     = "Terraform-cluster-${random_string.suffix.result}"
+}
