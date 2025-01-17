c48e6b3b5714d20f500bf6b1653afc95cdd99968
vaibhav jaiswal <vaibhavj@gmail.com>
2025-01-17 17:52:23 +0530

    Revert "done"
    This reverts commit d3c9400284ee0b85f9579de1057d7b06d830fd87

--- eks.tf
+++ eks.tf
@@ -244,17 +244,9 @@ output "kubeconfig" {
 
 
 
-
-# provider "aws" {
-#   region = var.region
-# }
-
-# data "aws_availability_zones" "available" {
-#   filter {
-#     name   = "opt-in-status"
-#     values = ["opt-in-not-required"]
-#   }
-# }
+provider "aws" {
+  region = var.region
+}
 
 # locals {
 #   cluster_name = "Terraform-cluster-${random_string.suffix.result}"
