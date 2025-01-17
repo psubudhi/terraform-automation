56479146de865830857f6be47d38a86e7803d4ec
vaibhav jaiswal <vaibhavj@gmail.com>
2025-01-17 18:53:20 +0530

    updated

--- eks.tf
+++ eks.tf
@@ -228,22 +228,6 @@ resource "null_resource" "get_kubeconfig" {
   }
 }
 
-# IAM Policy for EBS CSI Driver
-data "aws_iam_policy" "ebs_csi_policy" {
-  arn = "arn:aws:iam::aws:policy/service-role/AmazonEBSCSIDriverPolicy"
-}
-
-# IRSA for EBS CSI Driver
-module "irsa-ebs-csi" {
-  source  = "terraform-aws-modules/iam/aws//modules/iam-assumable-role-with-oidc"
-  version = "5.39.0"
-
-  create_role                   = true
-  role_name                     = "AmazonEKSTFEBSCSIRole-${module.eks.cluster_name}"
-  provider_url                  = module.eks.oidc_provider
-  role_policy_arns              = [data.aws_iam_policy.ebs_csi_policy.arn]
-  oidc_fully_qualified_subjects = ["system:serviceaccount:kube-system:ebs-csi-controller-sa"]
-}
 
 # Helm release for kube-prometheus-stack
 resource "helm_release" "kube_prometheus_stack" {
--- kube.tf
+++ kube.tf
@@ -1,11 +1,14 @@
-provider "helm" {
-  kubernetes = {
-    config_path = "${path.module}/kubeconfig"
-  }
-}
+# provider "helm" {
+#   kubernetes = {
+#     config_path = "${path.module}/kubeconfig"
+#   }
 
-provider "kubernetes" {
-  host                   = data.terraform_remote_state.eks.outputs.EKS_cluster_endpoint
-  cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.EKS_cluster_CA_data)
-  config_path            = "${path.module}/kubeconfig"
-}
+  
+# }
+
+# provider "kubernetes" {
+#   alias = "secondary"
+#   host                   = data.terraform_remote_state.eks.outputs.EKS_cluster_endpoint
+#   cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.EKS_cluster_CA_data)
+#   config_path            = "${path.module}/kubeconfig"
+# }
