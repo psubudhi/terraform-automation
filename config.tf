c672e35125cb2d1b24a5632304815d98042652f0
vaibhav jaiswal <vaibhavj@gmail.com>
2025-01-17 14:42:52 +0530

    changes config.tf

--- config.tf
+++ config.tf
@@ -1,3 +1,17 @@
+# provider "kubernetes" {
+#   host                   = module.eks.cluster_endpoint
+#   token                  = data.aws_eks_cluster_auth.eks.token
+#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+# }
+
+# data "aws_eks_cluster_auth" "eks" {
+#   name = module.eks.cluster_name
+# }
+
+# provider "helm" {
+#     config_path = "~/.kube/config"
+# }
+
 provider "kubernetes" {
   host                   = module.eks.cluster_endpoint
   token                  = data.aws_eks_cluster_auth.eks.token
@@ -10,7 +24,8 @@ data "aws_eks_cluster_auth" "eks" {
 
 provider "helm" {
   kubernetes {
-    config_path = "~/.kube/config"
+    host                   = module.eks.cluster_endpoint
+    token                  = data.aws_eks_cluster_auth.eks.token
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
   }
 }
-
