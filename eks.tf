80051b40b39431642c02b27ecdd4f8c26b6ff60f
vaibhav jaiswal <vaibhavj@gmail.com>
2025-01-17 16:54:43 +0530

    updated major changes

--- eks.tf
+++ eks.tf
@@ -1,16 +1,124 @@
 
+# provider "aws" {
+#   region = var.region
+# }
+
+# # Filter out local zones, which are not currently supported 
+# # with managed node groups
+# data "aws_availability_zones" "available" {
+#   filter {
+#     name   = "opt-in-status"
+#     values = ["opt-in-not-required"]
+#   }
+# }
+
+# locals {
+#   cluster_name = "Terraform-cluster-${random_string.suffix.result}"
+# }
+
+# resource "random_string" "suffix" {
+#   length  = 8
+#   special = false
+# }
+
+# module "vpc" {
+#   source  = "terraform-aws-modules/vpc/aws"
+#   version = "5.8.1"
+
+#   name = "education-vpc"
+
+#   cidr = "10.0.0.0/16"
+#   azs  = slice(data.aws_availability_zones.available.names, 0, 3)
+
+#   private_subnets = ["10.0.1.0/24", "10.0.2.0/24", "10.0.3.0/24"]
+#   public_subnets  = ["10.0.4.0/24", "10.0.5.0/24", "10.0.6.0/24"]
+
+#   enable_nat_gateway   = true
+#   single_nat_gateway   = true
+#   enable_dns_hostnames = true
+
+#   public_subnet_tags = {
+#     "kubernetes.io/role/elb" = 1
+#   }
+
+#   private_subnet_tags = {
+#     "kubernetes.io/role/internal-elb" = 1
+#   }
+# }
+
+# module "eks" {
+#   source  = "terraform-aws-modules/eks/aws"
+#   version = "20.8.5"
+
+#   cluster_name    = local.cluster_name
+#   cluster_version = "1.29"
+
+#   cluster_endpoint_public_access           = true
+#   enable_cluster_creator_admin_permissions = true
+
+#   cluster_addons = {
+#     aws-ebs-csi-driver = {
+#       service_account_role_arn = module.irsa-ebs-csi.iam_role_arn
+#     }
+#   }
+
+#   vpc_id     = module.vpc.vpc_id
+#   subnet_ids = module.vpc.private_subnets
+
+#   eks_managed_node_group_defaults = {
+#     ami_type = "AL2_x86_64"
+
+#   }
+
+#   eks_managed_node_groups = {
+#     one = {
+#       name = "node-group-1"
+
+#       instance_types = ["t2.medium"]
+
+#       min_size     = 1
+#       max_size     = 2
+#       desired_size = 1
+#     }
+#   }
+# }
+
+
+
+# resource "helm_release" "kube_prometheus_stack" {
+#   name             = "kube-prometheus-stack"
+#   chart            = "kube-prometheus-stack"
+#   repository       = "https://prometheus-community.github.io/helm-charts"
+#   namespace        = "monitoring"
+#   create_namespace = true
+#   version          = "67.10.0"
+
+#   # Ensure the Helm release depends on the EKS cluster being created first
+#   depends_on = [
+#     module.eks
+#   ]
+# }
+
+# # resource "null_resource" "helm_repo_update" {
+# #   provisioner "local-exec" {
+# #     command = "helm repo add prometheus-community https://prometheus-community.github.io/helm-charts && helm repo update"
+# #   }
+# # }
+
+
+
 provider "aws" {
   region = var.region
 }
 
 # Filter out local zones, which are not currently supported 
 # with managed node groups
-data "aws_availability_zones" "available" {
-  filter {
-    name   = "opt-in-status"
-    values = ["opt-in-not-required"]
-  }
-}
+# data "aws_availability_zones" "available" {
+#   filter {
+#     name   = "opt-in-status"
+#     values = ["opt-in-not-required"]
+#   }
+# }
 
 locals {
   cluster_name = "Terraform-cluster-${random_string.suffix.result}"
@@ -67,7 +175,6 @@ module "eks" {
 
   eks_managed_node_group_defaults = {
     ami_type = "AL2_x86_64"
-
   }
 
   eks_managed_node_groups = {
@@ -83,6 +190,23 @@ module "eks" {
   }
 }
 
+# Add a local-exec provisioner to get the kubeconfig path
+resource "null_resource" "get_kubeconfig" {
+  depends_on = [module.eks]
+
+  provisioner "local-exec" {
+    command = <<EOT
+      aws eks --region ${var.region} update-kubeconfig --name ${module.eks.cluster_name} --kubeconfig ${path.module}/kubeconfig
+    EOT
+  }
+}
+
+# Output the path of the kubeconfig file
+output "kubeconfig_path" {
+  value = "${path.module}/kubeconfig"
+}
+
+# Helm release for kube-prometheus-stack
 resource "helm_release" "kube_prometheus_stack" {
   name             = "kube-prometheus-stack"
   chart            = "kube-prometheus-stack"
@@ -91,16 +215,13 @@ resource "helm_release" "kube_prometheus_stack" {
   create_namespace = true
   version          = "67.10.0"
 
-  # Ensure the Helm release depends on the EKS cluster being created first
+  # Ensure the Helm release depends on the EKS cluster and kubeconfig path being set
   depends_on = [
-    module.eks
+    null_resource.get_kubeconfig
   ]
+
+  set {
+    name  = "kubeconfig"
+    value = "${path.module}/kubeconfig"
+  }
 }
-
-# resource "null_resource" "helm_repo_update" {
-#   provisioner "local-exec" {
-#     command = "helm repo add prometheus-community https://prometheus-community.github.io/helm-charts && helm repo update"
-#   }
-# }
-
-
--- kube.tf
+++ kube.tf
@@ -1,12 +1,12 @@
 provider "helm" {
   kubernetes = {
-    config_path = "~/.kube/config"
+    config_path = "${path.module}/kubeconfig"
   }
 }
  
 provider "kubernetes" {
   host                   = data.terraform_remote_state.eks.outputs.EKS_cluster_endpoint
   cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.EKS_cluster_CA_data)
-  config_path = "~/.kube/config"
+  config_path = "${path.module}/kubeconfig"
 # load_config_file       = false
 }
