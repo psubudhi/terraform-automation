ca2124492d14d355ff9fa02b0bd026ad59522a6c
vaibhav jaiswal <vaibhavj@gmail.com>
2025-01-17 18:46:20 +0530

    UPDATED EVERYTHING

--- config.tf
+++ config.tf
@@ -1,31 +1,18 @@
-# # provider "kubernetes" {
-# #   host                   = module.eks.cluster_endpoint
-# #   token                  = data.aws_eks_cluster_auth.eks.token
-# #   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-# # }
 
-# # data "aws_eks_cluster_auth" "eks" {
-# #   name = module.eks.cluster_name
-# # }
+provider "kubernetes" {
+  host                   = module.eks.cluster_endpoint
+  token                  = data.aws_eks_cluster_auth.eks.token
+  cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+}
 
-# # provider "helm" {
-# #     config_path = "~/.kube/config"
-# # }
+data "aws_eks_cluster_auth" "eks" {
+  name = module.eks.cluster_name
+}
 
-# provider "kubernetes" {
-#   host                   = module.eks.cluster_endpoint
-#   token                  = data.aws_eks_cluster_auth.eks.token
-#   cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-# }
-
-# data "aws_eks_cluster_auth" "eks" {
-#   name = module.eks.cluster_name
-# }
-
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.eks.cluster_endpoint
-#     token                  = data.aws_eks_cluster_auth.eks.token
-#     cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host                   = module.eks.cluster_endpoint
+    token                  = data.aws_eks_cluster_auth.eks.token
+    cluster_ca_certificate = base64decode(module.eks.cluster_certificate_authority_data)
+  }
+}
--- kube.tf
+++ kube.tf
@@ -3,10 +3,9 @@ provider "helm" {
     config_path = "${path.module}/kubeconfig"
   }
 }
- 
+
 provider "kubernetes" {
   host                   = data.terraform_remote_state.eks.outputs.EKS_cluster_endpoint
   cluster_ca_certificate = base64decode(data.terraform_remote_state.eks.outputs.EKS_cluster_CA_data)
-  config_path = "${path.module}/kubeconfig"
-# load_config_file       = false
+  config_path            = "${path.module}/kubeconfig"
 }
