815b57887fcf36cc6a5f977f4623ac5bee7e5f05
vaibhav jaiswal <vaibhavj@gmail.com>
2025-01-17 17:35:13 +0530

    ll

--- eks.tf
+++ eks.tf
@@ -374,6 +374,13 @@ provider "aws" {
   region = var.region
 }
 
+data "aws_availability_zones" "available" {
+  filter {
+    name   = "opt-in-status"
+    values = ["opt-in-not-required"]
+  }
+}
+
 locals {
   cluster_name = "Terraform-cluster-${random_string.suffix.result}"
 }
